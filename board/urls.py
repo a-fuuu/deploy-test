6b2cb2ecec476415d460c6cb9713dced9b2953e8
a-fuuu <vlvk22tpzjs@gmail.com>
2022-05-10 09:41:04 +0900

    id int로 수정

--- board/urls.py
+++ board/urls.py
@@ -3,7 +3,7 @@ from .views import*
 
 urlpatterns = [
     path('', home, name="home"),
-    path('<str:id>/', detail, name="detail"),
+    path('<int:id>/', detail, name="detail"),
     path('new/',new, name="new"),
     path('create/',create,name="create"),
     path('edit/<str:id>',edit,name="edit"),
